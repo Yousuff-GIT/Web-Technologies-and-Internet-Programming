95e5ba10310cba3c9560e17c454007b3576d33cd
Yousuff-GIT <yousuffrashid@gmail.com>
2025-06-14 10:22:34 +0530

    Added Exercise_21_Web_Service_for_Consumer_Product_Rating/src/SamsungReviews.java via uploader

---
+++ Exercise_21_Web_Service_for_Consumer_Product_Rating/src/SamsungReviews.java
@@ -0,0 +1,5 @@
+// Web Service class using JAX-WS annotations
+@WebService(serviceName = "SamsungReviews")
+public class SamsungReviews {
+    // This method calculates and returns the average rating
+}
